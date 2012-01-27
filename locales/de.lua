ea2e09a9fb9cb8acc00596534a1f21e0f4388e02
Lestat <1st@1st.1st>
2012-01-27 01:12:35 +0100

    changes of names of variables in local

--- locales/de.lua
+++ locales/de.lua
@@ -22,10 +22,10 @@ CP.L ={
     ERROR_PICKEDUPITEM = "There was a problem while scanning equipment! Check your current equipment!",
 
     PIMP_STAT = "Stat %i",
-    PIMP_RUNE = "Rune %i",
-    PIMP_DURA = "Dura",
+    PIMP_RUNE = "Rune %i",  -- SYS_ITEMTYPE_24
+    PIMP_DURA = "Dura",   --TEXT("_glossary_01188"),  -- Haltbarkeit
     PIMP_PLUS = "Plus",
-    PIMP_TIER = "Tier",
+    PIMP_TIER = TEXT("_glossary_00703"),
 
     BY_CARD = "Monstercards",
 
@@ -37,6 +37,8 @@ CP.L ={
         MAGIC = MAGIC,
 		--MELEE = MELEE -- Nahkampf
 		--RANGE = RANGE -- Fernkampf
+		--MAGIC_DEFENCE = TEXT("MAGIC_DEFENCE"),
+		--PHYSICAL_DEFENCE = TEXT("PHYSICAL_DEFENCE"),
 
         STR = C_STR,
         DEX = C_AGI,
@@ -44,20 +46,20 @@ CP.L ={
         INT = C_INT,
         WIS = C_MND,
 
-        HP = SYS_HEALTH,
-        MANA = SYS_MANA,
+        HP = TEXT("SYS_HEALTH"),
+        MANA = TEXT("SYS_MANA"),
 
         PDEF = C_PHYSICAL_DEFENCE,
         PARRY = C_PHYSICAL_PARRY,
         EVADE = C_PHYSICAL_DODGE,
-        PACC = "Accuracy",
+        PACC = TEXT("_glossary_00147"),
 
         MDEF = C_MAGIC_DEFENCE,
-        MRES = SYS_WEAREQTYPE_196,
+        MRES = TEXT("SYS_WEAREQTYPE_196"),
         MDMG = C_MAGIC_DAMAGE,
         MATK = C_MAGIC_ATTACK,
         MCRIT = C_MAGIC_CRITICAL,
         MHEAL = C_MAGIC_HEAL_POINT,
-        MACC = _glossary_00755 ,
+        MACC = TEXT("_glossary_00147") ,
     },
 }
