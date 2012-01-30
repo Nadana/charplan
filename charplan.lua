12e1038b3d36191a49667e343ff5ad79036e7167
McBen <viertelvor12@gmx.net>
2012-01-30 21:22:57 +0100

    another test item

--- charplan.lua
+++ charplan.lua
@@ -460,6 +460,12 @@ function CP.EquipItem_ShowMenu( this )
                 CP.Pimp.PimpItemLink("|Hitem:377a0 3 358e0c74 d41fd41a d56bd56d d48fd56c 0 0 0 0 2d50 82a0|h|cffc805f8[Ardmonds Helm]|r|h")
             end
         UIDropDownMenu_AddButton(info)
+
+        info.text   = string.format(CP.L.CONTEXT_PIMPME,"TEST - Flammendrachenherz-Kette der Intelligenz")
+        info.func   = function()
+                CP.Pimp.PimpItemLink("|Hitem:33e87 3 80b56 d2fed2ec d36cd305 d36fd36a 7eff9 0 0 0 4330 d408|h|cffc805f8[Flammendrachenherz-Kette der Intelligenz]|r|h")
+            end
+        UIDropDownMenu_AddButton(info)
         --@end-debug@
 
 
--- test/test_pimp.lua
+++ test/test_pimp.lua
@@ -7,6 +7,9 @@ function TestPimpMe:testLinks()
 
     local i = TestPimpMe:ExtractLink("|Hitem:377a0 3 358e0c74 d41fd41a d56bd56d d48fd56c 0 0 0 0 2d50 82a0|h|cffc805f8[Ardmonds Helm]|r|h")
     assertEquals(i.id,0x377a0)
+
+    i = TestPimpMe:ExtractLink("|Hitem:33e87 3 80b56 d2fed2ec d36cd305 d36fd36a 7eff9 0 0 0 4330 d408|h|cffc805f8[Flammendrachenherz-Kette der Intelligenz]|r|h")
+    assertEquals(i.id,0x33e87)
 end
 
 function TestPimpMe:ExtractLink(itemlink)
