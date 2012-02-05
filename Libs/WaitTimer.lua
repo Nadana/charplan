88732565dfb85a8df6bd810a253bbcf6d5c40039
McBen <viertelvor12@gmx.net>
2012-02-05 14:30:15 +0100

    using new "waittimer" lib

---
+++ Libs/WaitTimer.lua
@@ -0,0 +1,158 @@
+--[[
+    WaitTimer
+        A simple and easy timer class
+
+    by McBen
+
+
+    Init:
+        local WaitTimer = LibStub("WaitTimer")
+
+    Example:
+        -- simple delayed call
+        WaitTimer.Wait(5, function () DEFAULT_CHAT_FRAME:AddMessage("huhu") end)
+
+        -- repeatable call
+        id = WaitTimer.Wait(10, function ()
+                            DEFAULT_CHAT_FRAME:AddMessage("spam")
+                            return 10
+                        end)
+        ...
+        WaitTimer.Stop(id)
+
+
+    Usage:
+        timer_id = WaitTimer.Wait(seconds, function, id)
+        ================================================
+
+        - seconds=  how long to wait
+        - function= will be called when time is elapsed
+            if the function returns a value, this value is used as new wait_time.
+        - id= (optional) a fixed timer id
+            If another timer with the same ID exists it will be replaced
+
+        - timer_id= id which can be used in the other functions
+            It's equal "id" when it was provided
+
+
+        WaitTimer.Stop(id)
+        ===================
+        - stops the timer without calling the function
+
+
+        waittime = WaitTimer.Remaining(id)
+        ==================================
+        - @return remaining seconds
+                  or nil
+
+
+        WaitTimer.SetTime(id, delay)
+        ==================================
+        - reset the wait time
+        - if <delay> is ommited the function will be triggert on next update
+
+
+    Setup:
+        copy theses files to your addon or any subdirectory of it
+            LibStub.lua
+            WaitTimer.lua
+        and add them to the beginning of your .toc file
+
+--]]
+WaitTimerUpdateFrame = nil
+
+
+local Timer = LibStub:NewLibrary("WaitTimer", 1)
+if not Timer then return end
+
+
+
+
+Timer.events={}
+Timer.last_id=1000
+
+
+local function FindID(id)
+    for i,data in ipairs(Timer.events) do
+        if data[2]==id then return i,data end
+    end
+end
+
+local function StartUpdater()
+    if not WaitTimerUpdateFrame then
+        local frame = CreateUIComponent("Frame", "WaitTimerUpdateFrame", "UIParent")
+        frame:SetScripts("OnUpdate",[=[ WaitTimerOnUpdate(this,elapsedTime)]=])
+    end
+
+    WaitTimerUpdateFrame:Show()
+end
+
+local function StopUpdate()
+    if TimerUpdateFrame then
+        TimerUpdateFrame:Hide()
+    end
+end
+
+function WaitTimerOnUpdate(this,elapsedTime)
+
+    for i,data in ipairs(Timer.events) do
+
+        data[1] = data[1]-elapsedTime
+
+        if data[1]<=0 then
+            local next_delay = data[3]()
+            if next_delay then
+                data[1] = next_delay
+            else
+                table.remove(Timer.events,i)
+                if #Timer.events==0 then
+                    this:Hide()
+                end
+            end
+        end
+    end
+end
+
+function Timer.Wait(seconds, fct, id)
+    local _,cur_data = FindID(id)
+    if cur_data then
+        cur_data[1]=seconds
+        return
+    end
+
+    if not id then
+        id = Timer.last_id
+        Timer.last_id = Timer.last_id+1
+    end
+
+    table.insert(Timer.events, {seconds, id, fct} )
+
+    StartUpdater()
+
+    return id
+end
+
+function Timer.Remaining(id)
+    local _,cur_data = FindID(id)
+    if cur_data then
+        return cur_data[1]
+    end
+end
+
+function Timer.SetTime(id,delay)
+    local _,cur_data = FindID(id)
+    if cur_data then
+        cur_data[1] = (delay or 0)
+    end
+end
+
+function Timer.Stop(id)
+    local idx = FindID(id)
+    if idx then
+        table.remove(Timer.events,idx)
+
+        if #Timer.events==0 then
+            StopUpdate()
+        end
+    end
+end
