bc6f2fd770f57db6df9430d6e68a7af104473f3e
tasquer <luxtau@gmail.com>
2012-03-04 04:48:22 +0200

    merge

--- locales/ru.lua
+++ locales/ru.lua
@@ -8,10 +8,54 @@
 --]]
 
 CP.L =
---@localization(locale="esES", format="lua_table", handle-unlocalized="ignore")@
+{
+	BY_CLASS = "Базовое",
+	BY_SET = "От комплекта",
+	BY_SKILL = "От пассивок", -- Needs review
+	CONTEXT_CLEAR = "Очистить",
+	CONTEXT_MENU = "|cffb0b030[CharPlan]|r Добавить к снаряжению", -- Needs review
+	CONTEXT_PIMPME = "Улучшить %s",
+	CONTEXT_SEARCH = "Поиск предмета",
+	DLG_SAVENAME = "Название комплекта",
+	DLG_WAIT_INV = "Пожалуйста, подождите окончания осмотра вашего снаряжения", -- Needs review
+	ENUMERATE_CANCELED = "Осмотр прерван", -- Needs review
+	ERROR_NOSLOT = "Для импорта вашего снаряжения в аддон необходим хотя бы один свободный слот в рюкзаке", -- Needs review
+	ERROR_PICKEDUPITEM = "Возникла проблема при осмотре снаряжения, проверьте, не заблокирован ли какой-то предмет в нём", -- Needs review
+	ERROR_SEARCH_SKIN = "Skin is ambiguous and could be wrong!", -- Requires localization
+	MENU_CLEARALL = "Очистить комплект снаряжения",
+	MENU_DEL = "Удалить сохраненный",
+	MENU_IMPORT = "Импортировать снаряжение персонажа",
+	MENU_LOAD = "Загрузить снаряжение",
+	MENU_SAVE = "Сохранить",
+	MENU_SAVEAS = "Сохранить как…",
+	MENU_TITLE = "Действия", -- Needs review
+	PIMP_DURA = "Прочность",
+	PIMP_FILTER = "<фильтровать по названию или атрибуту>", -- Needs review
+	PIMP_PLUS = "Заточка", -- Needs review
+	PIMP_RUNE = "Руна %i",
+	PIMP_STAT = "Стат %i",
+	SEARCH_BASE_STATS = "Урон/ФЗ",
+	SEARCH_CONTEXT_SKIN = "В качестве внешки", -- Needs review
+	SEARCH_CONTEXT_TAKE = "Экипировать",
+	SEARCH_CONTEXT_WEB = "Открыть на сайте базы",
+	SEARCH_LEVEL = "Уровень", -- Needs review
+	SEARCH_NAME = "Название", -- Needs review
+	SEARCH_NOSTATLESS = "no empty items", -- Requires localization
+	SEARCH_STATS = "Статы",
+	SEARCH_TYPE = "Тип",
+	SEARCH_USE_SLOT1 = "В слот № 1",
+	SEARCH_USE_SLOT2 = "В слот № 2",
+	SEARCH_WEBSITE = "http://ru.runesdatabase.com/item/%i",
+	TITLE_EMPTY = "<без имени>",
+}
+
 
 CP.L.STAT_SHORTS =
---@localization(locale="esES", namespace="STAT_SHORTS", format="lua_table", handle-unlocalized="ignore")@
+{
+}
+
 
 CP.L.STAT_NAMES =
---@localization(locale="esES", namespace="STATS", format="lua_table", handle-unlocalized="ignore")@
+{
+}
+
